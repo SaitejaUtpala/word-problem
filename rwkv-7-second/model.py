343322848147d070083c08aab00010f5504944c4
Saiteja <saitejautpala@gmail.com>
2024-10-25 19:34:24 +0530

    add fix

--- rwkv-7-second/model.py
+++ rwkv-7-second/model.py
@@ -99,7 +99,11 @@ class RWKV_Tmix_x070(nn.Module):
         assert args.dim_att % self.n_head == 0
 
         with torch.no_grad():
-            ratio_0_to_1 = layer_id / (args.n_layer - 1)  # 0 to 1
+            if args.n_layer == 1:
+                ratio_0_to_1 = 1
+            else:
+                ratio_0_to_1 = layer_id / (args.n_layer - 1)
+
             ratio_1_to_almost0 = 1.0 - (layer_id / args.n_layer)  # 1 to ~0
             ddd = torch.ones(1, 1, args.n_embd)
             for i in range(args.n_embd):
