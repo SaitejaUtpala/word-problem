c1811fdf0f9de3a7da21e8b4ec6a87cc02714e1f
Saiteja <saitejautpala@gmail.com>
2024-10-27 11:27:58 +0530

    try and remove soem stuff

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for k in 20 20 20; do
-    for n in 1 2 3; do
-        for g in A5 A4_x_Z5; do
-            sbatch --export=ALL,k=$k,layers=$n,group=$g,batchsize=512,wd=0.01,samples=1000000,wsharing=False launch.slurm
-        done 
-    done
-done
